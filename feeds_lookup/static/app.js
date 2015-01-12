65102e7d5048f7977dd0e63a3b34580e8b5df605
walter brunetti <walterbrunetti@gmail.com>
2015-01-12 11:23:47 -0300

    app.js indentation fix

--- feeds_lookup/static/app.js
+++ feeds_lookup/static/app.js
@@ -1,22 +1,26 @@
-'use strict';
+(function () {
+    'use strict';
 
-var article_app = angular.module('article', ['ngRoute', 'article.controllers', 'article.services']);
+    var article_app = angular.module('article', ['ngRoute', 'article.controllers', 'article.services']);
 
-article_app.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/', {
-                templateUrl: '/static/article/assets/_articles.html',
-                controller: 'ArticleController'
-            }).
-            when('/:id', {
-                templateUrl: '/static/article/assets/_article_details.html',
-                controller: 'ArticleDetailController'
-            }).
-            otherwise({
-                redirectTo: '/'
-            });
-}]);
+    article_app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: '/static/article/assets/_articles.html',
+                    controller: 'ArticleController'
+                }).
+                when('/:id', {
+                    templateUrl: '/static/article/assets/_article_details.html',
+                    controller: 'ArticleDetailController'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
+    }]);
 
 
-//angular.bootstrap(document.getElementById("article-app"),['article']);
+    //angular.bootstrap(document.getElementById("article-app"),['article']);
+
+})();
+
